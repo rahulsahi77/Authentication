3c4192e0b10aecb9428d4c83e4c0fce47e409695
rahulsahi77 <sahirahul07@gmail.com>
2023-05-07 23:04:14 +0530

    Now using bcrypt

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
  
 const app = express();
 
@@ -39,9 +40,11 @@ app.get("/register", (req, res) => {
 // for the registration of users
 app.post("/register", async (req, res) => {
     try {
+        const salt = await bcrypt.genSalt(saltRounds);
+        const hash = await bcrypt.hash(req.body.password, salt);
         const newUser = new User({
             email: req.body.username,
-            password: md5(req.body.password)
+            password: hash
         });
         await newUser.save();
         res.render("secrets");
@@ -54,16 +57,18 @@ app.post("/register", async (req, res) => {
 // checking if the entered data matches with the database entries, if yes then let user log in
 app.post("/login", async (req, res) => {
     const username = req.body.username;
-    const password =  md5(req.body.password);
+    const password = req.body.password;
     try {
         const foundUser = await User.findOne({ email: username });
         if (foundUser) {
-            // Check if the password matches
-            if (password === foundUser.password) {
-                res.render("secrets");
-            } else {
-                res.status(401).send("Incorrect password");
-            }
+            // Check if the password matches using bcrypt.compare method
+            bcrypt.compare(password, foundUser.password, function(err, result) {
+                if (result === true) {
+                    res.render("secrets");
+                } else {
+                    res.status(401).send("Incorrect password");
+                }
+            });
         } else {
             res.status(404).send("User not found");
         }
