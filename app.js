bbd40f01786946e902384d37a8333390dd69dc84
rahulsahi77 <sahirahul07@gmail.com>
2023-05-07 22:17:39 +0530

    Hashing using md5

--- app.js
+++ app.js
@@ -4,19 +4,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-
-
+const md5 = require('md5');
+ 
 const app = express();
-
-console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-
 
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
@@ -25,12 +21,9 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
-
 const User = new mongoose.model("User", userSchema);
 
-
+// Requests----------------
 app.get("/", (req, res) => {
     res.render("home");
 });
@@ -48,7 +41,7 @@ app.post("/register", async (req, res) => {
     try {
         const newUser = new User({
             email: req.body.username,
-            password: req.body.password
+            password: md5(req.body.password)
         });
         await newUser.save();
         res.render("secrets");
@@ -61,7 +54,7 @@ app.post("/register", async (req, res) => {
 // checking if the entered data matches with the database entries, if yes then let user log in
 app.post("/login", async (req, res) => {
     const username = req.body.username;
-    const password = req.body.password;
+    const password =  md5(req.body.password);
     try {
         const foundUser = await User.findOne({ email: username });
         if (foundUser) {
@@ -80,10 +73,6 @@ app.post("/login", async (req, res) => {
     }
 });
 
-
-
-
-
 app.listen(3000, () => {
-    console.log("server started on port 3000.");
+    console.log("server started on port 3000."); 
 });
